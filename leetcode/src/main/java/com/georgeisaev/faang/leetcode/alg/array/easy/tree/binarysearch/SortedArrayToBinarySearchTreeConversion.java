841f19df71ca90558adb2f5230fde8ab4ba03549
Georgy Isaev <georgeisaev@gmail.com>
2021-12-11 20:30:42 +0300

    #1 108. Convert Sorted Array to Binary Search Tree Runtime: 0 ms, faster than 100.00% of Java online submissions for Convert Sorted Array to Binary Search Tree. Memory Usage: 38.7 MB, less than 78.74% of Java online submissions for Convert Sorted Array to Binary Search Tree.

---
+++ leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/tree/binarysearch/SortedArrayToBinarySearchTreeConversion.java
@@ -0,0 +1,7 @@
+package com.georgeisaev.faang.leetcode.alg.array.easy.tree.binarysearch;
+
+public interface SortedArrayToBinarySearchTreeConversion {
+
+	TreeNode sortedArrayToBST(int[] nums);
+
+}
---
+++ leetcode/src/test/java/com/georgeisaev/faang/leetcode/alg/array/easy/tree/binarysearch/SortedArrayToBinarySearchTreeConversionImplTest.java
@@ -0,0 +1,22 @@
+package com.georgeisaev.faang.leetcode.alg.array.easy.tree.binarysearch;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SortedArrayToBinarySearchTreeConversionImplTest {
+
+	SortedArrayToBinarySearchTreeConversion algorithm;
+
+	@BeforeEach
+	void setUp() {
+		algorithm = new SortedArrayToBinarySearchTreeConversionImpl();
+	}
+
+	@Test
+	void shouldConvert() {
+		// TODO: add unit test
+		// Assertions.assertEquals("[0,-10,5,null,-3,null,9]", algorithm.sortedArrayToBST(new int[]{-10, -3, 0, 5,
+		// 9}).toString());
+	}
+
+}
