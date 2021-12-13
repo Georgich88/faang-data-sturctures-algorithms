5670bef53b07567d02d73270ff56ec692e860fac
Georgy Isaev <georgeisaev@gmail.com>
2021-12-13 20:35:37 +0300

    #1 119. Pascal's Triangle II Runtime: 1 ms, faster than 77.23% of Java online submissions for Pascal's Triangle II. Memory Usage: 37.1 MB, less than 41.84% of Java online submissions for Pascal's Triangle II.

---
+++ leetcode/src/test/java/com/georgeisaev/faang/leetcode/alg/array/easy/pascal/triangle/PascalTriangleRowImplTest.java
@@ -0,0 +1,30 @@
+package com.georgeisaev.faang.leetcode.alg.array.easy.pascal.triangle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class PascalTriangleRowImplTest {
+
+    PascalTriangleRow algorithm;
+
+    @BeforeEach
+    void setUp() {
+        algorithm = new PascalTriangleRowImpl();
+    }
+
+    @Test
+    void shouldFindCombination() {
+        assertArrayEquals(new Integer[]{1}, algorithm.getRow(0).toArray(Integer[]::new));
+        assertArrayEquals(new Integer[]{1, 1}, algorithm.getRow(1).toArray(Integer[]::new));
+        assertArrayEquals(new Integer[]{1, 3, 3, 1}, algorithm.getRow(3).toArray(Integer[]::new));
+        assertArrayEquals(new Integer[]{1, 4, 6, 4, 1}, algorithm.getRow(4).toArray(Integer[]::new));
+        assertArrayEquals(new Integer[]{1, 5, 10, 10, 5, 1}, algorithm.getRow(5).toArray(Integer[]::new));
+        assertArrayEquals(new Integer[]{1, 33, 528, 5456, 40920, 237336, 1107568, 4272048, 13884156, 38567100,
+                92561040, 193536720, 354817320, 573166440, 818809200, 1037158320, 1166803110, 1166803110, 1037158320,
+                818809200, 573166440, 354817320, 193536720, 92561040, 38567100, 13884156, 4272048, 1107568, 237336,
+                40920, 5456, 528, 33, 1}, algorithm.getRow(33).toArray(Integer[]::new));
+    }
+
+}
--- leetcode/src/test/java/com/georgeisaev/faang/leetcode/alg/array/easy/pascal/triangle/PascalTriangleRowRecursiveImplTest.java
+++ leetcode/src/test/java/com/georgeisaev/faang/leetcode/alg/array/easy/pascal/triangle/PascalTriangleRowRecursiveImplTest.java
@@ -21,6 +21,10 @@ class PascalTriangleRowRecursiveImplTest {
 		assertArrayEquals(new Integer[]{1, 3, 3, 1}, algorithm.getRow(3).toArray(Integer[]::new));
 		assertArrayEquals(new Integer[]{1, 4, 6, 4, 1}, algorithm.getRow(4).toArray(Integer[]::new));
 		assertArrayEquals(new Integer[]{1, 5, 10, 10, 5, 1}, algorithm.getRow(5).toArray(Integer[]::new));
+		assertArrayEquals(new Integer[]{1, 33, 528, 5456, 40920, 237336, 1107568, 4272048, 13884156, 38567100,
+				92561040, 193536720, 354817320, 573166440, 818809200, 1037158320, 1166803110, 1166803110, 1037158320,
+				818809200, 573166440, 354817320, 193536720, 92561040, 38567100, 13884156, 4272048, 1107568, 237336,
+				40920, 5456, 528, 33, 1}, algorithm.getRow(33).toArray(Integer[]::new));
 	}
 
 }
