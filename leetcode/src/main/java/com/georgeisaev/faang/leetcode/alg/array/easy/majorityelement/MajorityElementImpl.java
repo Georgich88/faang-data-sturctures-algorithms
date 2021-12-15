ee2d7ebeac11e6c8fda02c74e5efb9cfdf65a5c6
Georgy Isaev <georgeisaev@gmail.com>
2021-12-15 23:16:14 +0300

    #1 169. Majority Element Runtime: 1 ms, faster than 99.90% of Java online submissions for Majority Element. Memory Usage: 42.1 MB, less than 93.76% of Java online submissions for Majority Element.

--- leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/majorityelement/MajorityElementImpl.java
+++ leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/majorityelement/MajorityElementImpl.java
@@ -1,7 +1,5 @@
 package com.georgeisaev.faang.leetcode.alg.array.easy.majorityelement;
 
-import java.util.Arrays;
-
 public class MajorityElementImpl implements MajorityElement {
 
     public static void main(String[] args) {
@@ -10,17 +8,15 @@ public class MajorityElementImpl implements MajorityElement {
 
     @Override
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
         int currentNumber = nums[0];
         int currentCount = 1;
-        final int majorityCount = nums.length / 2;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == currentNumber) {
                 currentCount++;
-                if (currentCount > majorityCount) {
-                    return currentNumber;
-                }
             } else {
+                currentCount--;
+            }
+            if (currentCount == 0) {
                 currentNumber = nums[i];
                 currentCount = 1;
             }
