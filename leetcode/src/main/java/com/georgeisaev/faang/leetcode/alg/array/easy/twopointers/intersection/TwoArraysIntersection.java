ee9a666283e0d2632bb8039e2830d2542a2f69a4
Isaev George <georgeisaev@gmail.com>
2022-01-11 07:16:38 +0300

    #1 349. Intersection of Two Arrays Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.

---
+++ leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/twopointers/intersection/TwoArraysIntersection.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.faang.leetcode.alg.array.easy.twopointers.intersection;
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection.
+ * Each element in the result must be unique, and you may return the result in any order.
+ * <p>
+ * <a href=https://leetcode.com/problems/intersection-of-two-arrays/>leetcode link</a>
+ */
+public interface TwoArraysIntersection {
+
+    int[] intersection(int[] nums1, int[] nums2);
+
+}
