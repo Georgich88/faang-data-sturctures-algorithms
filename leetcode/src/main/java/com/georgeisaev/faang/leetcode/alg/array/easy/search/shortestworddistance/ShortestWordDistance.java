89f72713eceff19f0451f520dcde00589b62f786
Isaev George <georgeisaev@gmail.com>
2022-01-16 13:29:53 +0300

    #1 243. Shortest Word Distance Runtime: 5 ms, faster than 10.49% of Java online submissions for Shortest Word Distance. Memory Usage: 41.7 MB, less than 18.94% of Java online submissions for Shortest Word Distance.

---
+++ leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/search/shortestworddistance/ShortestWordDistance.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.faang.leetcode.alg.array.easy.search.shortestworddistance;
+
+/**
+ * Given an array of strings wordsDict and two different strings that already exist in the array word1 and word2,
+ * return the shortest distance between these two words in the list.
+ * <p>
+ * <a href=https://leetcode.com/problems/shortest-word-distance/>leetcode link<a/>
+ */
+public interface ShortestWordDistance {
+
+    int shortestDistance(String[] wordsDict, String word1, String word2);
+
+}
