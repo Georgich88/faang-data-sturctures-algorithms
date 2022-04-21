12df5e4f63dcbfa6bfe5fad1791210335a6cb1b5
Исаев Георгий <isaev.georgii@otr.ru>
2022-04-21 10:31:02 +0300

    #1 35. Search Insert Position Runtime: 0 ms, faster than 100.00% of Java online submissions for Search Insert Position. Memory Usage: 43.3 MB, less than 56.36% of Java online submissions for Search Insert Position.

--- leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/search/insert/SearchInsertPositionImpl.java
+++ leetcode/src/main/java/com/georgeisaev/faang/leetcode/alg/array/easy/search/insert/SearchInsertPositionImpl.java
@@ -4,32 +4,20 @@ public class SearchInsertPositionImpl implements SearchInsertPosition {
 
 	@Override
 	public int searchInsert(int[] nums, int target) {
-		// Corner cases
-		if (nums.length == 1 && nums[0] >= target) {
-			return 0;
-		} else if (nums.length == 1) {
-			return 1;
-		}
 		int left = 0;
 		int right = nums.length - 1;
-		int position = 0;
-		while (left <= right) {
-			if (nums[left] >= target) {
-				position = left;
-				break;
+		int pivot;
+		while(left <= right) {
+			pivot = (right + left) >>> 1;
+			if (nums[pivot] < target) {
+				left = pivot + 1;
+			} else if (nums[pivot] > target) {
+				right = pivot - 1;
+			} else {
+				return pivot;
 			}
-			if (nums[right] == target) {
-				position = right;
-				break;
-			} else if (nums[right] < target) {
-				position = right + 1;
-				break;
-			}
-			position = target - nums[left] <= target - nums[right] ? left : right;
-			left++;
-			right--;
 		}
-		return position;
+		return left;
 	}
 
 }
